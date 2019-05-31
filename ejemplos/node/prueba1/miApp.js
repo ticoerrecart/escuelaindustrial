a1e910be7494519cf1a863b68691936fc3ea18f9
rodolfo.errecart <rodolfo.errecart@arba.gov.ar>
2019-05-31 15:32:01 -0300

    node express

--- ejemplos/node/prueba1/miApp.js
+++ ejemplos/node/prueba1/miApp.js
@@ -1,14 +1,48 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 
+var data = [{"nombre": "Mario","edad":"15", "duenios":[{"nombre":"Juan"},{"nombre":"Gregoria"}]},
+	          {"nombre": "Flunfli","edad":"5", "duenios":[{"nombre":"Bautista"},{"nombre":"Maria"}]}
+			];
+
+function buscar(nombre){
+	var elem = data.filter(function(elem) {
+	  return elem.nombre.toLowerCase().includes(nombre.toLowerCase());
+	});
+	
+	return elem;
+}
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
+  bodyParser.json();
+  
+  next();//paso el control al próximo handler (si hubiera varios en la cadena) con esta funcion
+});
+
+app.use(express.bodyParser());
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Bienvenido a la Tienda de Mascotas!');
 });
 
 app.get('/mascotas', function (req, res) {
-	res.json([{"nombre": "Mario","edad":"15", "duenios":[{"nombre":"Juan"},{"nombre":"Gregoria"}]},
-	          {"nombre": "Flunfli","edad":"5", "duenios":[{"nombre":"Bautista"},{"nombre":"Maria"}]}
-			]);
+	res.json(data);
+});
+
+app.get('/mascotasBuscar/:texto', function (req, res) {
+	console.log(req.params.texto);
+	var data = buscar(req.params.texto)
+	res.json(data);
+});
+
+app.post('/mascotasGuardar', function (req, res) {
+	var miMascotaNueva = req.body;
+	console.log(miMascotaNueva);
 });
 
 
