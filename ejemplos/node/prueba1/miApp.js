83462ca3eb9156c3063e0a468fee3d6d50ee194a
rodolfo <tico@tico.com.ar>
2019-06-07 15:22:40 -0300

    nodejs + express + jquery +  get/post

--- ejemplos/node/prueba1/miApp.js
+++ ejemplos/node/prueba1/miApp.js
@@ -1,30 +1,42 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+
 var app = express();
+
+app.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  
+	  next();//paso el control al próximo handler (si hubiera varios en la cadena) con esta funcion
+	});
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 
 var data = [{"nombre": "Mario","edad":"15", "duenios":[{"nombre":"Juan"},{"nombre":"Gregoria"}]},
 	          {"nombre": "Flunfli","edad":"5", "duenios":[{"nombre":"Bautista"},{"nombre":"Maria"}]}
 			];
 
 function buscar(nombre){
+	/*data.forEach(function(elem){
+		console.log(elem.nombre);
+	});*/
 	var elem = data.filter(function(elem) {
-	  return elem.nombre.toLowerCase().includes(nombre.toLowerCase());
+		var nombreData = elem.nombre.toLowerCase();
+		//console.log(nombreData.indexOf(nombre.toLowerCase()));
+	  return nombreData.indexOf(nombre.toLowerCase())>=0;
 	});
 	
 	return elem;
 }
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  
-  bodyParser.json();
-  
-  next();//paso el control al próximo handler (si hubiera varios en la cadena) con esta funcion
-});
 
-app.use(express.bodyParser());
+
 
 app.get('/', function (req, res) {
   res.send('Bienvenido a la Tienda de Mascotas!');
@@ -43,6 +55,8 @@ app.get('/mascotasBuscar/:texto', function (req, res) {
 app.post('/mascotasGuardar', function (req, res) {
 	var miMascotaNueva = req.body;
 	console.log(miMascotaNueva);
+	data.push(miMascotaNueva);
+	res.json({"mensaje":"Se han guardado correctamente los datos"});
 });
 
 
